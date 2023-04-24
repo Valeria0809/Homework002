1f34b36d2b99105f9e5e1e63961b12e36d42a1f7
VALERIA <valeri.tereshenkova@icloud.com>
2023-04-25 00:45:24 +0300

    Upload Task 13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число: ");
+int Value2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = Value2.ToString().Length;
+if (Length2 >= 3) {
+	while (Value2 > 999)
+	{
+		Value2 = Value2 / 10;
+	}
+	int result = Value2 % 10;
+	Console.WriteLine("Третья цифра " + result);
+} else {
+	Console.WriteLine("Третьей цифры нет");
+}
